3e5f73a84422055d93a3de0296fe176101ac506a
eugene-pande <esp.pandev@gmail.com>
2025-08-18 10:09:59 +1000

    Install Laravel breeze for Authentication

--- config/app.php
+++ config/app.php
@@ -17,6 +17,22 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+     /*
+    |--------------------------------------------------------------------------
+    | Application Version
+    |--------------------------------------------------------------------------
+    |
+    | This value is the version of your application. This value is used when the
+    | framework needs to place the application's version in a notification or
+    | any other location as required by the application or its packages.
+    |
+    |
+    |
+    */
+
+    'version' => env('APP_VERSION', '2.0.0'),
+
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
@@ -194,6 +210,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\VoltServiceProvider::class,
 
     ],
 
