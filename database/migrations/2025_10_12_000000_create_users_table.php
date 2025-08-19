11de9dd58dd9d7e631e06dd42b6576c2743809b5
eugene-pande <esp.pandev@gmail.com>
2025-08-19 14:34:52 +1000

    Updated user migration table

--- database/migrations/2025_10_12_000000_create_users_table.php
+++ database/migrations/2025_10_12_000000_create_users_table.php
@@ -13,10 +13,22 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('uuid')->unique();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->unsignedBigInteger('user_type_id');
+            $table->foreign('user_type_id')->references('id')->on('user_types')->onDelete('restrict');
+
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            
+            $table->unsignedBigInteger('account_status_id');
+            $table->foreign('account_status_id')->references('id')->on('account_statuses')->onDelete('cascade');
+
             $table->rememberToken();
             $table->timestamps();
         });
