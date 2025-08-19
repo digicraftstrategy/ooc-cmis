bbdd69b6e0ddfb63472f156a3dd3a07ad2e12b77
eugene-pande <esp.pandev@gmail.com>
2025-08-19 12:49:10 +1000

    Facotored middleware in the route files

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,28 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-Route::get('regions', \App\Livewire\Admin\Regions\RegionsTable::class)->name('system.regions');
-Route::get('regions/register', \App\Livewire\Admin\Regions\CreateRegion::class)->name('system.regions.create');
-Route::get('regions/edit/{id}', \App\Livewire\Admin\Regions\EditRegion::class)->name('system.regions.edit');
-Route::get('regions/view/{id}', \App\Livewire\Admin\Regions\ViewRegion::class)->name('system.regions.view');
-
+// Accessed by authenticated users
+Route::group(
+    [
+        'middleware' => ['auth', 'verified'],
+    ],
+    function () {
+        // Region
+        Route::prefix('regions')
+            ->group(function () {
+                Route::get('/', \App\Livewire\Admin\Regions\RegionsTable::class)->name('system.regions');
+                Route::get('/register', \App\Livewire\Admin\Regions\CreateRegion::class)->name('system.regions.create');
+                Route::get('/edit/{id}', \App\Livewire\Admin\Regions\EditRegion::class)->name('system.regions.edit');
+                Route::get('/view/{id}', \App\Livewire\Admin\Regions\ViewRegion::class)->name('system.regions.view');
+            });
+            // Province
+        ROute::prefix('provinces')
+            ->group(function () {
+                Route::get('/', \App\Livewire\Admin\Province\ProvinceTable::class)->name('system.provinces');
+                Route::get('/register', \App\Livewire\Admin\Province\CreateProvince::class)->name('system.provinces.create');
+                Route::get('/edit/{id}', \App\Livewire\Admin\Province\EditProvince::class)->name('system.provinces.edit');
+                Route::get('/view/{provinceId}', \App\Livewire\Admin\Province\ViewProvince::class)->name('system.provinces.view');
+            });
+    }
+);
 require __DIR__.'/auth.php';
