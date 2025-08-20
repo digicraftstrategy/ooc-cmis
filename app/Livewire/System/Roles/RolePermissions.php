8f7b275f361c7f8b0ffc211385ff7ce2b30d0517
eugene-pande <esp.pandev@gmail.com>
2025-08-20 14:19:08 +1000

    Updated Rolestable component to display roles

---
+++ app/Livewire/System/Roles/RolePermissions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\System\Roles;
+
+use Livewire\Component;
+
+class RolePermissions extends Component
+{
+    public function render()
+    {
+        return view('livewire.system.roles.role-permissions');
+    }
+}
