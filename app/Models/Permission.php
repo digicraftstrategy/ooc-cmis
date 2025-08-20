c418925712b3f6f4ae15a6f6a412808f6bb628b5
eugene-pande <esp.pandev@gmail.com>
2025-08-20 16:48:55 +1000

    Updated role and permission models

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Permission extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'permissions';
+    protected $fillable = ['name', 'slug', 'description', 'group'];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_permissions', 'permission_id', 'role_id');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_permissions', 'permission_id', 'user_id');
+    }
 }
