d6062396f31afc4e397593450729eab46017e7b5
eugene-pande <esp.pandev@gmail.com>
2025-09-30 11:40:46 +1000

    Factored website url to the manage owers view

--- resources/views/livewire/admin/publication-premises/premises-owner/manage-premises.blade.php
+++ resources/views/livewire/admin/publication-premises/premises-owner/manage-premises.blade.php
@@ -108,9 +108,13 @@
                                             <svg class="w-4 h-4 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"></path>
                                             </svg>
-                                            <a href="https://www.ownerbusiness.com" target="_blank" class="text-blue-600 hover:text-blue-800 font-medium transition-colors duration-200">
-                                                {{$premisesOwner->owners_name}}
-                                            </a>
+                                            @if($premisesOwner->website)
+                                                <a href="{{ $premisesOwner->website }}" target="_blank" class="text-blue-600 hover:text-blue-800 font-medium transition-colors duration-200">
+                                                    {{ $premisesOwner->website }}
+                                                </a>
+                                            @else
+                                                <span class="text-gray-400 font-medium">No website Address Provided</span>
+                                            @endif
                                         </div>
                                     </div>
                                 </div>
