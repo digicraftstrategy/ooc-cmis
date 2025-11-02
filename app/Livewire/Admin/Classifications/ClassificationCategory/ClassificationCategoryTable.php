3cd404bfa80fd3ddecc0f5ae66e134edb35e1868
eugene-pande <esp.pandev@gmail.com>
2025-11-03 09:01:24 +1000

    Updated ClassificationCategory component with the render method to display classification categories

--- app/Livewire/Admin/Classifications/ClassificationCategory/ClassificationCategoryTable.php
+++ app/Livewire/Admin/Classifications/ClassificationCategory/ClassificationCategoryTable.php
@@ -2,12 +2,44 @@
 
 namespace App\Livewire\Admin\Classifications\ClassificationCategory;
 
+use App\Models\ClassificationCategory;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ClassificationCategoryTable extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+    public $sortField = 'name';
+    public $sortDirection = 'asc';
+    public $perPage = 10;
+    public $classification_categories;
+
+    public function sortBy($field)
+    {
+        if ($this->sortField === $field) {
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->sortField = $field;
+            $this->sortDirection = 'asc';
+        }
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        return view('livewire.admin.classifications.classification-category.classification-category-table');
+        $paginateClassificationCategories = ClassificationCategory::where('name', 'like', '%' . $this->search . '%')
+            ->orWhere('slug', 'like', '%' . $this->search . '%')
+            ->orderBy($this->sortField, $this->sortDirection)
+            ->paginate($this->perPage);
+
+        return view('livewire.admin.classifications.classification-category.classification-category-table',[
+            'classification_categories' => $$paginateClassificationCategories,
+        ]);
     }
 }
