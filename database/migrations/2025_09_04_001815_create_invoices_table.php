656f26544b76797a7385a86ede234a2c99c59c1a
eugene-pande <esp.pandev@gmail.com>
2025-11-18 00:20:13 +1000

    Updated invoice migration table

--- database/migrations/2025_09_04_001815_create_invoices_table.php
+++ database/migrations/2025_09_04_001815_create_invoices_table.php
@@ -16,17 +16,25 @@ return new class extends Migration
             $table->string('invoice_number')->unique();
             $table->date('invoice_date');
             $table->date('due_date');
-            $table->decimal('subtotal', 10, 2)->default(0.00);
-            $table->decimal('tax', 10, 2)->default(0.00);
-            $table->decimal('total', 10, 2)->default(0.00);
+            $table->decimal('amount', 10,2)->default(0.00); // This amount will come from the amount from the activity using the foreign key reference
+            $table->decimal('subtotal', 10, 2)->default(0.00); // This is the total after calculating from the amount
+            $table->decimal('tax', 10, 2)->default(0.00); // Tax
+            $table->decimal('total', 10, 2)->default(0.00); // Amount after tax
+
+            $table->string('billing_email'); // Can come from the premsies if no new email if provided
+            $table->string('billing_address')->nullable(); // Can come from premsiess if no new address is provided
+
             $table->enum('status', ['pending', 'paid', 'cancelled','overdue'])->default('pending');
             $table->text('notes')->nullable();
 
             $table->unsignedBigInteger('premises_id');
             $table->foreign('premises_id')->references('id')->on('premises')->onDelete('cascade');
 
-            //$table->unsignedBigInteger('activity_id');
-            //$table->foreign('activity_id')->references('id')->on('premises_activities')->onDelete('cascade');
+            $table->unsignedBigInteger('activity_id');
+            $table->foreign('activity_id')->references('id')->on('premises_activities')->onDelete('cascade');
+
+            $table->unsignedBigInteger('classification_item_id');
+            $table->foreign('classification_item_id')->references('id')->on('classifications')->cascadeOnDelete();
             $table->timestamps();
 
             //$table->index(['premises_id', 'activity_id']);
